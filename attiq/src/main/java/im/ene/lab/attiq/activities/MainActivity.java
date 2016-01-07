faa6d55d23d7ea68168ba14889fe6be5c2800bbd
Nguyen Hoai Nam <mr.nguyenhoainam@gmail.com>
2016-01-07 15:27:57 +0900

    Various fix

--- attiq/src/main/java/im/ene/lab/attiq/activities/MainActivity.java
+++ attiq/src/main/java/im/ene/lab/attiq/activities/MainActivity.java
@@ -32,14 +32,14 @@ import de.greenrobot.event.EventBus;
 import im.ene.lab.attiq.Attiq;
 import im.ene.lab.attiq.R;
 import im.ene.lab.attiq.data.api.ApiClient;
-import im.ene.lab.attiq.util.event.Event;
-import im.ene.lab.attiq.util.event.ProfileFetchedEvent;
-import im.ene.lab.attiq.data.two.Profile;
 import im.ene.lab.attiq.data.two.AccessToken;
+import im.ene.lab.attiq.data.two.Profile;
 import im.ene.lab.attiq.fragment.FeedListFragment;
 import im.ene.lab.attiq.fragment.PublicStreamFragment;
 import im.ene.lab.attiq.util.PrefUtil;
 import im.ene.lab.attiq.util.UIUtil;
+import im.ene.lab.attiq.util.event.Event;
+import im.ene.lab.attiq.util.event.ProfileFetchedEvent;
 import im.ene.lab.attiq.widgets.RoundedTransformation;
 import io.realm.Realm;
 import retrofit2.Callback;
@@ -50,20 +50,26 @@ public class MainActivity extends BaseActivity
 
   public static final int LOGIN_REQUEST_CODE = 0xa11d;
   public static final String EXTRA_AUTH_CALLBACK = "extra_auth_callback";
-
-  private Realm mRealm;
-  private View mHeaderView;
-
-  private DrawerLayout mDrawerLayout;
-  private TabLayout mMainTabs;
-
-  private Fragment mFragment;
-
   @Bind(R.id.header_account_background) View mHeaderBackground;
   @Bind(R.id.header_account_icon) ImageView mHeaderIcon;
   @Bind(R.id.header_account_name) TextView mHeaderName;
   @Bind(R.id.header_account_description) TextView mHeaderDescription;
   @Bind(R.id.header_auth_menu) ImageButton mAuthMenu;
+  int mIconCornerRadius;
+  int mIconBorderWidth;
+  int mIconBorderColor;
+  // No ButterKnife
+  Toolbar mToolBar;
+  View mContainer;
+  ViewPager mViewPager;
+  MenuItem mAuthMenuItem;
+  private Realm mRealm;
+  private View mHeaderView;
+  private DrawerLayout mDrawerLayout;
+  private TabLayout mMainTabs;
+  private Fragment mFragment;
+  private NavigationView mNavigationView;
+  private Profile mMyProfile;
 
   @OnClick(R.id.header_auth_menu) void toggleAuthMenu() {
     if (mAuthMenuItem != null) {
@@ -74,19 +80,6 @@ public class MainActivity extends BaseActivity
       }
     }
   }
-
-  int mIconCornerRadius;
-  int mIconBorderWidth;
-  int mIconBorderColor;
-
-  // No ButterKnife
-  Toolbar mToolBar;
-  View mContainer;
-  ViewPager mViewPager;
-
-  private NavigationView mNavigationView;
-
-  MenuItem mAuthMenuItem;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -134,115 +127,16 @@ public class MainActivity extends BaseActivity
 
     mRealm = Attiq.realm();
 
-    Profile user = mRealm.where(Profile.class).findFirst();
-    if (user == null) {
-      user = mRealm.where(Profile.class)
-          .equalTo("token", PrefUtil.getCurrentToken()).findFirst();
-    }
+    mMyProfile = mRealm.where(Profile.class)
+        .equalTo("token", PrefUtil.getCurrentToken()).findFirst();
 
-    if (user != null) {
-      updateMasterUser(user);
+    if (mMyProfile != null) {
+      updateMasterUser(mMyProfile);
     }
 
     trySetupToolBarTabs();
 
     getMasterUser(PrefUtil.getCurrentToken());
-  }
-
-  @Override
-  public void onBackPressed() {
-    if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-      mDrawerLayout.closeDrawer(GravityCompat.START);
-    } else {
-      super.onBackPressed();
-    }
-  }
-
-  private void login() {
-    Intent intent = new Intent(this, WebViewActivity.class);
-    startActivityForResult(intent, LOGIN_REQUEST_CODE);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  @SuppressWarnings("StatementWithEmptyBody")
-  @Override public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
-    int id = item.getItemId();
-    // TODO navigation
-    if (id == R.id.nav_login) {
-      login();
-    }
-
-    mDrawerLayout.closeDrawer(GravityCompat.START);
-    return true;
-  }
-
-  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (resultCode == Activity.RESULT_OK && requestCode == LOGIN_REQUEST_CODE && data != null) {
-      String callback = data.getStringExtra(EXTRA_AUTH_CALLBACK);
-      Uri callbackUri = Uri.parse(callback);
-      final String code = callbackUri.getQueryParameter("code");
-      ApiClient.accessToken(code).enqueue(new Callback<AccessToken>() {
-        @Override public void onResponse(Response<AccessToken> response) {
-          AccessToken accessToken = response.body();
-          if (accessToken != null) {
-            PrefUtil.setCurrentToken(accessToken.getToken());
-            getMasterUser(accessToken.getToken());
-          }
-        }
-
-        @Override public void onFailure(Throwable t) {
-
-        }
-      });
-    }
-  }
-
-  private void getMasterUser(final String token) {
-    ApiClient.me().enqueue(new Callback<Profile>() {
-      @Override public void onResponse(Response<Profile> response) {
-        Profile profile = response.body();
-        if (profile != null) {
-          profile.setToken(token);
-          Realm realm = Attiq.realm();
-          realm.beginTransaction();
-          realm.copyToRealmOrUpdate(profile);
-          realm.commitTransaction();
-          realm.close();
-          EventBus.getDefault().post(new ProfileFetchedEvent(true, null, profile));
-        } else {
-          EventBus.getDefault().post(new ProfileFetchedEvent(false,
-              new Event.Error(response.code(), response.message()), null));
-        }
-      }
-
-      @Override public void onFailure(Throwable error) {
-        EventBus.getDefault().post(new ProfileFetchedEvent(false,
-            new Event.Error(Event.Error.ERROR_UNKNOWN, error.getLocalizedMessage()), null));
-      }
-    });
   }
 
   private void updateMasterUser(@NonNull Profile user) {
@@ -262,21 +156,6 @@ public class MainActivity extends BaseActivity
           .transform(new RoundedTransformation(
               mIconBorderWidth, mIconBorderColor, mIconCornerRadius))
           .into(mHeaderIcon);
-    }
-  }
-
-  public void onEventMainThread(final ProfileFetchedEvent event) {
-    if (event.success) {
-      if (PrefUtil.isFirstStart()) {
-        PrefUtil.setFirstStart(false);
-        mDrawerLayout.openDrawer(GravityCompat.START);
-      }
-
-      trySetupToolBarTabs();
-
-      if (event.profile != null) {
-        updateMasterUser(event.profile);
-      }
     }
   }
 
@@ -343,12 +222,127 @@ public class MainActivity extends BaseActivity
     mToolBar.addView(mMainTabs, lp);
   }
 
+  private void getMasterUser(final String token) {
+    ApiClient.me().enqueue(new Callback<Profile>() {
+      @Override public void onResponse(Response<Profile> response) {
+        mMyProfile = response.body();
+        if (mMyProfile != null) {
+          mMyProfile.setToken(token);
+          Realm realm = Attiq.realm();
+          realm.beginTransaction();
+          realm.copyToRealmOrUpdate(mMyProfile);
+          realm.commitTransaction();
+          realm.close();
+          EventBus.getDefault().post(new ProfileFetchedEvent(true, null, mMyProfile));
+        } else {
+          EventBus.getDefault().post(new ProfileFetchedEvent(false,
+              new Event.Error(response.code(), response.message()), null));
+        }
+      }
+
+      @Override public void onFailure(Throwable error) {
+        EventBus.getDefault().post(new ProfileFetchedEvent(false,
+            new Event.Error(Event.Error.ERROR_UNKNOWN, error.getLocalizedMessage()), null));
+      }
+    });
+  }
+
   @Override protected void onDestroy() {
     if (mRealm != null) {
       mRealm.close();
     }
     ButterKnife.unbind(this);
     super.onDestroy();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+    // TODO navigation
+    if (id == R.id.nav_login) {
+      login();
+    } else if (id == R.id.nav_profile) {
+      if (mMyProfile != null) {
+        startActivity(ProfileActivity.createIntent(this, mMyProfile.getId()));
+      }
+    }
+
+    mDrawerLayout.closeDrawer(GravityCompat.START);
+    return true;
+  }
+
+  private void login() {
+    Intent intent = new Intent(this, WebViewActivity.class);
+    startActivityForResult(intent, LOGIN_REQUEST_CODE);
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == Activity.RESULT_OK && requestCode == LOGIN_REQUEST_CODE && data != null) {
+      String callback = data.getStringExtra(EXTRA_AUTH_CALLBACK);
+      Uri callbackUri = Uri.parse(callback);
+      final String code = callbackUri.getQueryParameter("code");
+      ApiClient.accessToken(code).enqueue(new Callback<AccessToken>() {
+        @Override public void onResponse(Response<AccessToken> response) {
+          AccessToken accessToken = response.body();
+          if (accessToken != null) {
+            PrefUtil.setCurrentToken(accessToken.getToken());
+            getMasterUser(accessToken.getToken());
+          }
+        }
+
+        @Override public void onFailure(Throwable t) {
+
+        }
+      });
+    }
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+      mDrawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  public void onEventMainThread(final ProfileFetchedEvent event) {
+    if (event.success) {
+      if (PrefUtil.isFirstStart()) {
+        PrefUtil.setFirstStart(false);
+        mDrawerLayout.openDrawer(GravityCompat.START);
+      }
+
+      trySetupToolBarTabs();
+
+      if (event.profile != null) {
+        updateMasterUser(event.profile);
+      }
+    }
   }
 
   private static class MainPagerAdapter extends FragmentStatePagerAdapter {
