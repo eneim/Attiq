4c043befd050fc861593e40283c8eefccf77acb1
eneim <1776230+eneim@users.noreply.github.com>
2020-08-10 18:01:30 +0900

    New dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@
 
 plugins {
   id("com.android.application")
+  id("androidx.navigation.safeargs")
   kotlin("android")
   kotlin("kapt")
 }
@@ -30,6 +31,8 @@ android {
     targetSdkVersion(30)
     versionCode(1)
     versionName("1.0")
+
+    buildConfigField("String", "BASE_URL", "\"https://qiita.com\"")
 
     testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
   }
@@ -53,6 +56,13 @@ android {
   buildFeatures {
     viewBinding = true
   }
+
+  configurations {
+    implementation.get().exclude(
+      group = "org.jetbrains",
+      module = "annotations"
+    )
+  }
 }
 
 dependencies {
@@ -73,8 +83,15 @@ dependencies {
   implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
   implementation("androidx.recyclerview:recyclerview:1.2.0-alpha05")
   implementation("androidx.constraintlayout:constraintlayout:2.0.0-rc1")
+
   implementation("androidx.paging:paging-common-ktx:3.0.0-alpha04")
   implementation("androidx.paging:paging-runtime-ktx:3.0.0-alpha04")
+
+  // implementation("androidx.room:room-common:2.3.0-alpha02")
+  // kapt("androidx.room:room-compiler:2.3.0-alpha02")
+  // implementation("androidx.room:room-coroutines:2.3.0-alpha02")
+  // implementation("androidx.room:room-runtime:2.3.0-alpha02")
+  // implementation("androidx.room:room-ktx:2.3.0-alpha02")
 
   implementation("com.squareup.okhttp3:okhttp:4.8.1")
   implementation("com.squareup.okhttp3:logging-interceptor:4.8.1")
@@ -85,6 +102,8 @@ dependencies {
   implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
   implementation("io.coil-kt:coil:0.11.0")
+  implementation("com.samskivert:jmustache:1.15")
+  implementation("org.jsoup:jsoup:1.13.1")
 
   testImplementation("junit:junit:4.13")
   androidTestImplementation("androidx.test.ext:junit:1.1.1")
